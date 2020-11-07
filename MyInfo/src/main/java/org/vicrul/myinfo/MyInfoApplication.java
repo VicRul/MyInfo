a94d79885403c7d9c0a1edffa710094d7478d989
victor.sotskov <vic.sotskov@gmail.com>
2020-11-07 18:20:47 +0700

    create project

---
+++ MyInfo/src/main/java/org/vicrul/myinfo/MyInfoApplication.java
@@ -0,0 +1,13 @@
+package org.vicrul.myinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInfoApplication.class, args);
+	}
+
+}
---
+++ MyInfo/src/test/java/org/vicrul/myinfo/MyInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vicrul.myinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
